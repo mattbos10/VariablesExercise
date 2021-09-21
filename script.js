de6a5e79a9860bca356f2f4de20f5276a2ddcdc2
Matt Boston <mattbos10@icloud.com>
2021-09-21 14:13:12 -0500

    all

---
+++ script.js
@@ -0,0 +1,19 @@
+//  const
+let num = 28;
+console.log(num);
+num = 15;
+console.log(num);
+num = num + 7;
+console.log(num);
+num --;
+console.log(num);
+num = 'this is not a number';
+console.log(num);
+let blank = null ;
+console.log(blank);
+// BONUS
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+console.log(quarter, dime, nickel, penny);
+let letters = 'qwertyuiop';
+console.log(letters);
+console.log(letters.slice(-1));
